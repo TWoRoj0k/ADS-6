658b5a6d6d6ef58bb82e45bc48c46b2033a6d3a6
TWoRoj0k <madgamerpro331@gmail.com>
2026-06-07 15:06:37 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,102 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    Node(const T& value) : data(value), next(nullptr) {}
+  };
 
-struct SYM {
-  char ch;
-  int prior;
+  Node* head_;
+  Node* tail_;
+  int size_;
+
+ public:
+  TPQueue() : head_(nullptr), tail_(nullptr), size_(0) {}
+
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+
+  // Добавление элемента в очередь с учётом приоритета
+  void push(const T& item) {
+    Node* newNode = new Node(item);
+
+    // Если очередь пуста
+    if (isEmpty()) {
+      head_ = newNode;
+      tail_ = newNode;
+      size_++;
+      return;
+    }
+
+    // Если приоритет нового элемента выше приоритета головы
+    if (newNode->data.prior > head_->data.prior) {
+      newNode->next = head_;
+      head_ = newNode;
+      size_++;
+      return;
+    }
+
+    // Поиск позиции для вставки
+    Node* current = head_;
+    while (current->next != nullptr &&
+           current->next->data.prior >= newNode->data.prior) {
+      current = current->next;
+    }
+
+    // Вставка в найденную позицию
+    newNode->next = current->next;
+    current->next = newNode;
+
+    // Если вставили в конец, обновляем tail
+    if (newNode->next == nullptr) {
+      tail_ = newNode;
+    }
+
+    size_++;
+  }
+
+  // Извлечение элемента из начала очереди (самый высокий приоритет)
+  T pop() {
+    if (isEmpty()) {
+      return T();
+    }
+
+    Node* temp = head_;
+    T result = head_->data;
+    head_ = head_->next;
+
+    // Если очередь стала пустой, обновляем tail
+    if (head_ == nullptr) {
+      tail_ = nullptr;
+    }
+
+    delete temp;
+    size_--;
+    return result;
+  }
+
+  // Просмотр первого элемента без удаления
+  T front() const {
+    if (isEmpty()) {
+      return T();
+    }
+    return head_->data;
+  }
+
+  // Проверка на пустоту
+  bool isEmpty() const {
+    return head_ == nullptr;
+  }
+
+  // Текущий размер очереди
+  int size() const {
+    return size_;
+  }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
